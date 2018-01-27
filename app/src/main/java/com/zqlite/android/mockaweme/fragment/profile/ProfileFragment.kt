1d8dd712cbb1cc6852b5bb5903bf1aab0b289e30
Scott <qinglian.zhang@dewmobile.net>
2018-01-27 16:12:48 +0800

    change icon and app name

--- app/src/main/java/com/zqlite/android/mockaweme/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/zqlite/android/mockaweme/fragment/profile/ProfileFragment.kt
@@ -4,10 +4,8 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.graphics.Rect
 import android.os.Bundle
-import android.support.design.widget.AppBarLayout
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
